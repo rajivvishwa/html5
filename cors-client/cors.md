c17e60dff3a3ca4ec2c6cc535c98885bb6ccb231
Rajiv Vishwa <rajivvishwa@gmail.com>
2013-06-11 17:03:58 -0700

    Cors Client

---
+++ cors-client/cors.md
@@ -0,0 +1,7 @@
+Reference Links
+===============
+- Tutorial from HTML Rocks : http://www.html5rocks.com/en/tutorials/cors/
+- Enabling CORS: http://enable-cors.org
+
+	Client: http://enable-cors.org/client.html
+	Server: http://enable-cors.org/server.html
---
+++ cors-client/js/corsCtrl.js
@@ -0,0 +1,21 @@
+angular.module('Test', ['ngResource']).controller('corsCtrl', function ($scope, $http, $resource) {
+    $http.defaults.useXDomain = true;
+
+    $scope.useHttp = function() {
+        $http.get('../../cors-server/')
+            .success(function(data) {
+                alert(data.ok);
+            });
+    };
+
+    $scope.useResource = function() {
+        var User = $resource('http://ricardohbin.com/cors/testcors.php', {
+            userId: '@id'
+        });
+        User.get({
+            userId: 123
+        }, function(data) {
+            alert(data.ok);
+        });
+    };
+});
