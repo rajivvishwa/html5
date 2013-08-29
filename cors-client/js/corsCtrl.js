f162b2578df7afe52cb368c2fe9f73db3b8df8ae
Rajiv Vishwa <rajivvishwa@gmail.com>
2013-08-29 10:28:09 -0700

    Updated Cors Client URL

--- cors-client/js/corsCtrl.js
+++ cors-client/js/corsCtrl.js
@@ -7,16 +7,18 @@ angular.module('CorsTest', ['ngResource']).controller('corsCtrl', function ($sco
      * so the server is not rejecting the incoming request.
      */
 
+    // var urlA2="../cors-server/test.json"; 
+    //  var urlA1="http://ricardohbin.com/cors/testcors.php";
+
     delete $http.defaults.headers.common['X-Requested-With'];
 
-    var urlA="../cors-server/test.json"; 
-    var urlA1="http://ricardohbin.com/cors/testcors.php";
-    var urlA2="http://appsec.kd.io/cors/";
+
+    var urlA="http://rajivvishwa.kd.io/appsec/html5/cors-server/";
     var urlNA="http://api.twitter.com/help/test.json";
     var CORS_ERROR = "Error - Cors Not Enabled";
         
     $scope.isAllowed = function() {
-        $http.get(url=urlA2)
+        $http.get(url=urlA)
             .success(function(data,status) {
                 $scope.corsData = data;
                 $scope.corsURL = url;
